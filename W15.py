22eb5ee557f0bb6a0ffe437b6c2044bf883ff7b8
Raj Chaliparambil <chaliparambil@chapman.edu>
2023-12-04 10:39:28 -0800

    Create W15.py

---
+++ W15.py
@@ -0,0 +1,8 @@
+print("Hey there! Can you please enter your name?")
+name = ("Enter: ")
+
+print("Now can you enter your birthday? (Ex. 02/14/2005)")
+birthday =("Enter: ")
+
+print(f"\nHappy Birthday, {name}! 🎉🎂")
+print(f"We hope you have a fantastic day on {birthday}!")
